1471ee2b473d53114aa67f792c38b89b8dcb59d5
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-01 15:12:52 -0400

    Add getDates function that inserts the current date and the next 5 days

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Call getDates function to get dates
+getDates();
+
+
 // When a user enters in a city and clicks search, it calls the makeRequest function
 $("#btnSearch").on("click", function(){
     var cityInput = $("#cityInput").val();
@@ -32,18 +36,35 @@ function getWeather(data){
 
         // Get the city name, wind speed, uv index for the current day
         var city = data.city.name;
-        $(".cityName").text(city);
+        $(".cityName").append(city);
 
         var windSpeed = data.list[0].wind.speed;
-        $("#windSpeed").text(windSpeed + " mph")
+        $("#day0 .windSpeed").text(windSpeed + " mph");
+
+        // var uvIndex = data.list[0].
 
 
         // Get the tempurature and humidity for current day and next 5 days
         for(var i = 0; i < 2; i++){
             var temp = Math.round(data.list[i].main.temp);
-            $("#temp" + i).text(temp + " F");
+            $("#day" + i + " .temp").text(temp + " F");
 
             var humidity = data.list[i].main.humidity;
-            $("#humid" + i).text(humidity + "%");
+            $("#day" + i + " .humid").text(humidity + "%");
         }
+}
+
+
+// Get the dates for today and the next 5 days
+function getDates(){
+    var now = moment().format("dddd, MMMM Do YYYY");
+    $("#currentDate").text(now);
+
+    // Get next five days and insert them into the weather forecast cards
+    for(var i = 1; i < 2; i++){
+        var day = moment().add(1, 'days');
+        console.log(day);
+        
+        // $("#day" + i + " .date").text(day);
+    }
 }
