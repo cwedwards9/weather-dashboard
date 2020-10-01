8943922a0f713fa0f67fab7870ba4e92dbcda1a1
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-01 13:44:23 -0400

    Add another event listener for clicking cities in the list history and making a ajax request to weather api. Add a new function for making the ajax request that is called by both event listeners of search input and list clicking.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// b007a7abea7f47541c213f81d9379014
-
-// When a user enters in a city and clicks search, a GET request is made and gets the weather data from openweathermap api
+// When a user enters in a city and clicks search, it calls the makeRequest function
 $("#btnSearch").on("click", function(){
-    var city = $("#cityInput").val();
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=b007a7abea7f47541c213f81d9379014";
+    var cityInput = $("#cityInput").val();
+
+    makeRequest(cityInput);
+});
+
+// When a user clicks on a city in the history list, it calls the makeRequest function 
+$("ul").on("click", "li", function(){
+    var cityList = $(this).text();
+
+    makeRequest(cityList);
+})
+
+// A GET request is made and gets the weather data (from openweather api) for the city parameter passed in
+function makeRequest(city){
+    var cityName = city;
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=b007a7abea7f47541c213f81d9379014";
     console.log(weatherUrl);
 
     // Make ajax call
@@ -11,15 +23,19 @@ $("#btnSearch").on("click", function(){
         method: "GET",
         url: weatherUrl
     })
-    .done(function(data){
-        console.log(data);
+    .done(getWeather)
+}
+
+// Takes the requested weather data and inserts it into the html for the current day and the next 5 days 
+function getWeather(data){
+    console.log(data);
 
         // Get the city name, wind speed, uv index for the current day
         var city = data.city.name;
         $(".cityName").text(city);
 
         var windSpeed = data.list[0].wind.speed;
-        $("#windSpeed").text(windSpeed + "mph")
+        $("#windSpeed").text(windSpeed + " mph")
 
 
         // Get the tempurature and humidity for current day and next 5 days
@@ -30,6 +46,4 @@ $("#btnSearch").on("click", function(){
             var humidity = data.list[i].main.humidity;
             $("#humid" + i).text(humidity + "%");
         }
-        
-    })
-});
+}
